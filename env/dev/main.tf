8d35a3f9a2e5ddb681824a95ce821268fdb22005
ando_kazuki <azarasikazuki@gmail.com>
2024-06-25 00:54:06 +0900

    save

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -9,3 +9,22 @@ module "entory" {
   network         = local.network
   jump_server     = local.jump_server
 }
+
+//sample
+# module "jump_server" {
+#   source = "../iam_role"
+
+#   resource_prefix = var.resource_prefix
+#   usage_name      = "jump-server"
+#   identifier      = "ec2.amazonaws.com"
+#   policy_json     = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = []
+#         Resource = []
+#       }
+#     ]
+#   })
+# }
